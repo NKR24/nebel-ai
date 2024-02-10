aff4bdf12da29b491d5dc8a68631b67023c0a998
Nikita Khvorostin <khvorostinn@gmail.com>
2024-02-10 20:03:10 +0100

    update starfield

--- gui/src/components/Starfield/Starfield.tsx
+++ gui/src/components/Starfield/Starfield.tsx
@@ -4,10 +4,9 @@ export function Starfield() {
   return (
     <div class={styles.starfield}>
       <div class={styles.stars} />
-      <div class={styles.stars} />
-      <div class={styles.stars} />
-      <div class={styles.stars} />
-      <div class={styles.stars} />
+      <div class={styles.stars1} />
+      <div class={styles.stars2} />
+      <div class={styles.stars3} />
     </div>
   )
 }
