e67be03ef2791866e65e592757d8e0d29ef68945
Nikita Khvorostin <khvorostinn@gmail.com>
2024-02-14 06:19:38 +0100

    remove starfield --push

--- gui/src/app.tsx
+++ gui/src/app.tsx
@@ -5,22 +5,18 @@ import { FileRoutes } from "@solidjs/start"
 import { Suspense } from "solid-js"
 import "./app.scss"
 import Navbar from "~/components/Navbar/Navbar"
-import { Starfield } from "~/components/Starfield/Starfield"
 
 export default function App() {
   return (
-    <>
-      <Starfield />
-      <Router
-        root={(props) => (
-          <MetaProvider>
-            <Navbar />
-            <Suspense>{props.children}</Suspense>
-          </MetaProvider>
-        )}
-      >
-        <FileRoutes />
-      </Router>
-    </>
+    <Router
+      root={(props) => (
+        <MetaProvider>
+          <Navbar />
+          <Suspense>{props.children}</Suspense>
+        </MetaProvider>
+      )}
+    >
+      <FileRoutes />
+    </Router>
   )
 }
--- gui/src/components/Starfield/Starfield.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Starfield.module.scss"
-
-export function Starfield() {
-  return (
-    <div class={styles.starfield}>
-      <div class={styles.stars} />
-    </div>
-  )
-}
