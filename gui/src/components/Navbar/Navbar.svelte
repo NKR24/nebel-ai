ea1d8c86662782a7ad29e46360f29af31ffaab5b
Nikita Khvorostin <khvorostinn@gmail.com>
2024-02-16 02:53:07 +0300

    added mediaqueri

--- gui/src/components/Navbar/Navbar.svelte
+++ gui/src/components/Navbar/Navbar.svelte
@@ -1,11 +1,6 @@
-<script>
-	import styles from './Navbar.module.scss';
-	import { page } from '$app/stores';
-</script>
-
-<nav class={styles.nav}>
-	<div class={styles.container}>
-		<div aria-current={$page.url.pathname === '/' ? 'page' : undefined} class={styles.logo}>
+<nav class="nav">
+	<div class="container">
+		<div class="logo">
 			<a href="/">
 				<svg
 					width="40"
@@ -21,17 +16,118 @@
 				</svg>
 			</a>
 		</div>
-		<div class={styles.pages}>
-			<div aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">
-					<span class={styles.text}>About</span>
-				</a>
-			</div>
-			<div aria-current={$page.url.pathname === '/portfolio' ? 'page' : undefined}>
-				<a href="/portfolio">
-					<span class={styles.text}>Portfolio</span>
-				</a>
-			</div>
+		<div class="pages">
+			<a href="/about">
+				<span class="text">About</span>
+			</a>
+			<a href="/portfolio">
+				<span class="text">Portfolio</span>
+			</a>
 		</div>
 	</div>
 </nav>
+
+<style lang="scss">
+
+	@media (max-width: 768px) {
+		.nav{
+			display: none;
+		}
+  }
+
+    .nav {
+        flex-direction: column;
+        display: flex;
+        height: 100vh;
+        z-index: 2;
+        padding: 42px 58px 0;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+    }
+
+    @media (max-width: 768px) {
+        .nav {
+            padding: 100px 15px 12px 12px;
+            flex-direction: column;
+            justify-content: flex-end;
+            bottom: 0;
+        }
+    }
+
+    .container {
+        height: 76px;
+        position: relative;
+
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+
+        border-radius: 13px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
+        background-color: rgba(19, 22, 31, 0.9);
+        backdrop-filter: blur(6px);
+        -webkit-backdrop-filter: blur(16px);
+
+        @media (max-width: 768px) {
+            height: 50px;
+        }
+    }
+
+    .logo {
+        position: absolute;
+        left: 20px;
+        top: 19px;
+
+        @media (max-width: 768px) {
+            left: 17px;
+            top: 0;
+        }
+    }
+
+    .logo svg {
+        @media (max-width: 768px) {
+            width: 30px;
+            height: 50px;
+        }
+    }
+
+    .pages {
+        z-index: 10;
+        display: flex;
+        column-gap: 17px;
+
+        @media (max-width: 768px) {
+            position: absolute;
+            right: 20px;
+            column-gap: 13px;
+        }
+    }
+
+    .text {
+        opacity: 1;
+        color: rgba(255, 255, 255, 1);
+        font-size: 24px;
+        font-weight: 700;
+        text-align: center;
+        text-decoration: underline;
+
+        @media (max-width: 768px) {
+            opacity: 1;
+            color: rgba(255, 255, 255, 1);
+            font-size: 16px;
+            font-weight: 700;
+            text-align: center;
+            text-decoration: underline;
+        }
+    }
+
+
+    @media (max-width: 768px) {
+        .nav {
+
+        }
+    }
+</style>
