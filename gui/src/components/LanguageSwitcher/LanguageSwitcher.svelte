de9f78af7aaaa42cfdb2427e325bd8b170718a23
Nikita Khvorostin <khvorostinn@gmail.com>
2024-03-16 01:53:10 +0100

    implement language switcher

---
+++ gui/src/components/LanguageSwitcher/LanguageSwitcher.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+  import { availableLanguageTags, languageTag } from "$paraglide/runtime"
+  import { i18n } from "../../utils/i18n.svelte"
+  import { page } from "$app/stores"
+  import { goto } from "$app/navigation"
+
+  function handleLanguageSwitch(event: Event) {
+    const url = $page.url.pathname.replace(
+      new RegExp(`/${availableLanguageTags.join("|")}`, "i"),
+      ""
+    )
+
+    goto(
+      i18n.resolveRoute(
+        url,
+        (event?.currentTarget as unknown as { value: string })
+          ?.value as (typeof availableLanguageTags)[number]
+      )
+    )
+  }
+</script>
+
+<select value={languageTag()} onchange={handleLanguageSwitch}>
+  {#each availableLanguageTags as language}
+    <option value={language} selected={language === languageTag()}
+      >{language}</option
+    >
+  {/each}
+</select>
+
+<style lang="scss">
+  option:hover {
+    opacity: 0;
+  }
+
+  option {
+    all: unset;
+    color: white;
+    padding: 20px;
+    border-bottom: 1px solid #ccc;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
+    background-color: rgb(19, 22, 31);
+  }
+
+  select {
+    all: unset;
+    cursor: pointer;
+    margin-right: 10px;
+    position: absolute;
+    right: 20px;
+    top: 19px;
+    border-radius: 5px;
+    height: 40px;
+
+    opacity: 1;
+    color: rgba(255, 255, 255, 1);
+    font-size: 24px;
+    font-weight: 700;
+    text-align: center;
+    text-decoration: underline;
+
+    appearance: none;
+    -webkit-appearance: none;
+
+    @media (max-width: 768px) {
+      right: 17px;
+      top: 6px;
+    }
+  }
+</style>
--- gui/src/components/Navbar/Navbar.svelte
+++ gui/src/components/Navbar/Navbar.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { route } from "../../consts"
   import { t } from "../../utils/i18n.svelte"
+  import LanguageSwitcher from "../LanguageSwitcher/LanguageSwitcher.svelte"
 </script>
 
 <nav class="navbar-container">
@@ -29,53 +30,11 @@
         <span class="text">{t.navbar$portfolio()}</span>
       </a>
     </div>
+    <LanguageSwitcher />
   </div>
 </nav>
 
 <style lang="scss">
-  option:hover {
-    opacity: 0;
-  }
-
-  button {
-    position: absolute;
-    width: 30px;
-    height: 30px;
-  }
-
-  option {
-    all: unset;
-    color: white;
-    padding: 20px; /* Отступы */
-    border-bottom: 1px solid #ccc;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
-    background-color: rgb(19, 22, 31);
-  }
-
-  button {
-    all: unset;
-    margin-right: 10px;
-    position: absolute;
-    right: 20px;
-    top: 19px;
-    border-radius: 5px;
-    height: 40px;
-
-    opacity: 1;
-    color: rgba(255, 255, 255, 1);
-    font-size: 24px;
-    font-weight: 700;
-    text-align: center;
-    text-decoration: underline;
-
-    appearance: none; /* Убираем стандартные стили браузера */
-    -webkit-appearance: none; /* Для Safari и Chrome */
-    @media (max-width: 768px) {
-      right: 17px;
-      top: 6px;
-    }
-  }
-
   .navbar-container {
     padding: 42px 58px 0;
     position: fixed;
