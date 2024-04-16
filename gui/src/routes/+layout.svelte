064a2639e925290b8c2dcb42db78896266dc3768
Nikita Khvorostin <khvorostinn@gmail.com>
2024-04-16 21:24:41 +0200

    confirm successful CI test

--- gui/src/routes/+layout.svelte
+++ gui/src/routes/+layout.svelte
@@ -6,6 +6,7 @@
   import { languageTag } from "$paraglide/runtime"
   import { loadCyrillicFont, loadJapaneseFont } from "../utils/css"
   import "../app.scss"
+  import Starfield from "../components/Starfield/Starfield.svelte"
 
   $effect(() => {
     if (languageTag() === "ru") {
@@ -17,6 +18,8 @@
     }
   })
 </script>
+
+<Starfield />
 
 <ParaglideJS {i18n}>
   <Navbar />
