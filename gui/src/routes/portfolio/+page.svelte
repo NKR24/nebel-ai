0c60a2c659f1f14cc328651ffe037dedf12cb1a6
Nikita Khvorostin <khvorostinn@gmail.com>
2024-04-17 02:07:08 +0300

    revert to a89a51b014ac54b7d34dd9e726fab3e678e7c8d5 + added hbh portfolio

--- gui/src/routes/portfolio/+page.svelte
+++ gui/src/routes/portfolio/+page.svelte
@@ -4,16 +4,29 @@
   import styleHelpers from "$utils/helpers.module.scss"
   import nahIdLive from "$img/nahIdLive.png.webp"
   import sadRuby from "$img/sadRuby.gif"
+  import hbhLogo from "$img/hbh.png.webp"
 </script>
 
 <PageWrapper title={t.portfolio$portfolio()} maxWidth="80ch">
   <h1 class={styleHelpers.title}>{t.portfolio$heading()}</h1>
   <img src={nahIdLive} alt="Strongest idol" class="nahIdLive" />
+
+  <div class="card">
+    <h1 class={styleHelpers.title}>{t.portfolio$hbhHeading()}</h1>
+    <a href="hbh.nebel.ai">
+      <img src={hbhLogo} alt="Strongest idol" class="nahIdLive" />
+    </a>
+  </div>
+
   <p>{t.portfolio$message()}</p>
   <img src={sadRuby} alt="Sad Ruby" class="sadRuby" />
 </PageWrapper>
 
 <style>
+  .card {
+    margin-top: 50px;
+  }
+
   .nahIdLive {
     max-width: 95%;
     padding-top: 16px;
